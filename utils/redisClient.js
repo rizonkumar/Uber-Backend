3fb611f40aaeb07972674c6f0ae11aee1f1692e3
Rizon Rahi <rizon.kumar.rahi@gmail.com>
2024-11-02 22:53:15 +0530

    Feat: Implemented Db and Redis

---
+++ utils/redisClient.js
@@ -0,0 +1,18 @@
+const redis = require("redis");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const redisClient = redis.createClient({
+  url: process.env.REDIS_URL,
+});
+
+redisClient.on("connect", () => {
+  console.log("Redis connected");
+});
+
+redisClient.on("error", (err) => {
+  console.log("Redis error: ", err);
+});
+
+module.exports = redisClient;
