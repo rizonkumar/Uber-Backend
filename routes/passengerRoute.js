b6ae0cef9cfce299534e21d4d45f1607415a478d
Rizon Rahi <rizon.kumar.rahi@gmail.com>
2024-11-03 09:51:17 +0530

    Feat: Impelemented Passenger Controller and Service

--- routes/passengerRoute.js
+++ routes/passengerRoute.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const authMiddleware = require("../middlewares/authMiddleware");
+const {
+  getPassengerBookings,
+  provideFeedback,
+} = require("../controllers/passengerController");
 
 const router = express.Router();
 
 module.exports = (io) => {
-  router.get("/bookings", authMiddleware, getPassingBookings);
+  router.get("/bookings", authMiddleware, getPassengerBookings);
   router.post("/feedback", authMiddleware, provideFeedback);
 
   return router;
